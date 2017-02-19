cd1412d19fc16118a24d58129ec8e2fa1a3b8a82
ganga <ganga@ganga-PC>
2017-02-19 14:22:01 +0530

    dfdgdg

---
+++ src/myprojet/launchfirefox.java
@@ -0,0 +1,15 @@
+package myprojet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class launchfirefox {
+
+	public static void main(String[] args)
+	{
+		WebDriver driver =new FirefoxDriver();
+		//driver.get("https://www.google.com");
+
+	}
+
+}
